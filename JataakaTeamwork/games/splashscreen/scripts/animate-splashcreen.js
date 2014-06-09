8d6a5e70ca96cf590d1b29d070b2f40e2991655b
Georgi Prodanov <georgiwe@gmail.com>
2014-06-09 13:08:06 +0300

    Add menu skeleton and improved animation timings.

--- JataakaTeamwork/games/splashscreen/scripts/animate-splashcreen.js
+++ JataakaTeamwork/games/splashscreen/scripts/animate-splashcreen.js
@@ -2,9 +2,11 @@
 
     var presents = $('#presents');
     var title = presents.next();
+    var small = title.next();
 
     presents.css('display', 'block').hide();
     title.css('display', 'block').hide();
+    small.css('display', 'block').hide();
 
     presents.fadeIn(4000);
     presents.fadeOut(1500);
@@ -14,10 +16,13 @@
     }, 5500);
 
     setTimeout(function () {
+        small.fadeIn(1000);
         var doc = $(document);
         doc.on('click', function () {
             title.fadeOut(2000);
             doc.off('click');
+            $('#menu-wrapper').fadeIn(6500);
+            small.fadeOut(1000);
         });
     }, 9500)
 })()
