f4d6607826e42961148158830199797ac7ad8f31
Georgi Prodanov <georgiwe@gmail.com>
2014-06-09 12:32:53 +0300

    Add game splashscreen

---
+++ JataakaTeamwork/games/splashscreen/scripts/animate-splashcreen.js
@@ -0,0 +1,23 @@
+﻿(function myfunction() {
+
+    var presents = $('#presents');
+    var title = presents.next();
+
+    presents.css('display', 'block').hide();
+    title.css('display', 'block').hide();
+
+    presents.fadeIn(4000);
+    presents.fadeOut(1500);
+
+    setTimeout(function () {
+        title.fadeIn(4000);
+    }, 5500);
+
+    setTimeout(function () {
+        var doc = $(document);
+        doc.on('click', function () {
+            title.fadeOut(2000);
+            doc.off('click');
+        });
+    }, 9500)
+})()
---
+++ JataakaTeamwork/games/splashscreen/scripts/script.js
@@ -0,0 +1,21 @@
+﻿(function () {
+
+    var win = $(window);
+    var scWrapper = $('#start-screen-wrapper');
+    var wrapperWidth = scWrapper.width();
+    var wrapperHeight = scWrapper.height();
+
+    function centerStartScreen() {
+        var winWidth = win.width();
+        var winHeight = win.height();
+        var marginTop = (winHeight - wrapperHeight) / 2;
+
+        scWrapper.css({
+            left: (winWidth - wrapperWidth) / 2,
+            top: marginTop * 0.60
+        });
+    }
+
+    centerStartScreen();
+    win.resize(centerStartScreen)
+})()
