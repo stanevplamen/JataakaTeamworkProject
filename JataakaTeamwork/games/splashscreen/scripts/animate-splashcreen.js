c7c95c94af5d3915215b0faf9e219821256666d1
Georgi Prodanov <georgiwe@gmail.com>
2014-06-10 18:00:01 +0300

    Disable "development mode" on animations.

--- JataakaTeamwork/games/splashscreen/scripts/animate-splashcreen.js
+++ JataakaTeamwork/games/splashscreen/scripts/animate-splashcreen.js
@@ -7,37 +7,37 @@
     var menuWrapper = $('#menu-wrapper');
 
     // comment this out, uncoment whats below
-    menuWrapper.fadeIn(500);
-    centerMenuWrapper();
-    win.resize(centerMenuWrapper);
+    //menuWrapper.fadeIn(500);
+    //centerMenuWrapper();
+    //win.resize(centerMenuWrapper);
 
-    //presents.css('display', 'block').hide();
-    //title.css('display', 'block').hide();
-    //small.css('display', 'block').hide();
+    presents.css('display', 'block').hide();
+    title.css('display', 'block').hide();
+    small.css('display', 'block').hide();
 
-    //presents.fadeIn(4000);
-    //presents.fadeOut(1500);
+    presents.fadeIn(4000);
+    presents.fadeOut(1500);
 
-    //setTimeout(function () {
-    //    title.fadeIn(4000);
-    //}, 5500);
+    setTimeout(function () {
+        title.fadeIn(4000);
+    }, 5500);
 
-    //setTimeout(function () {
-    //    small.fadeIn(1000);
-    //    var doc = $(document);
+    setTimeout(function () {
+        small.fadeIn(1000);
+        var doc = $(document);
 
-    //    doc.on('click', function () {
-    //        title.fadeOut(2000);
-    //        doc.off('click');
-    //        win.off('resize');
+        doc.on('click', function () {
+            title.fadeOut(2000);
+            doc.off('click');
+            win.off('resize');
 
-    //        centerMenuWrapper();
-    //        win.resize(centerMenuWrapper);
+            centerMenuWrapper();
+            win.resize(centerMenuWrapper);
 
-    //        menuWrapper.fadeIn(6500);
-    //        small.fadeOut(1000);
-    //    });
-    //}, 9500)
+            menuWrapper.fadeIn(6500);
+            small.fadeOut(1000);
+        });
+    }, 9500)
 
     function centerMenuWrapper() {
         var winWidth = win.width();
