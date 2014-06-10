3502a4fe131eaba540677fd750a59f36575494c6
Georgi Prodanov <georgiwe@gmail.com>
2014-06-10 17:57:12 +0300

    Upload Authors content. Add animations to content and toggle. Adjust animation timings and content centering upon resize.

--- JataakaTeamwork/games/splashscreen/scripts/animate-splashcreen.js
+++ JataakaTeamwork/games/splashscreen/scripts/animate-splashcreen.js
@@ -6,38 +6,54 @@
     var win = $(window);
     var menuWrapper = $('#menu-wrapper');
 
-    presents.css('display', 'block').hide();
-    title.css('display', 'block').hide();
-    small.css('display', 'block').hide();
+    // comment this out, uncoment whats below
+    menuWrapper.fadeIn(500);
+    centerMenuWrapper();
+    win.resize(centerMenuWrapper);
 
-    presents.fadeIn(4000);
-    presents.fadeOut(1500);
+    //presents.css('display', 'block').hide();
+    //title.css('display', 'block').hide();
+    //small.css('display', 'block').hide();
 
-    setTimeout(function () {
-        title.fadeIn(4000);
-    }, 5500);
+    //presents.fadeIn(4000);
+    //presents.fadeOut(1500);
 
-    setTimeout(function () {
-        small.fadeIn(1000);
-        var doc = $(document);
+    //setTimeout(function () {
+    //    title.fadeIn(4000);
+    //}, 5500);
 
-        doc.on('click', function () {
-            title.fadeOut(2000);
-            doc.off('click');
-            win.off('resize');
+    //setTimeout(function () {
+    //    small.fadeIn(1000);
+    //    var doc = $(document);
 
-            centerMenuWrapper();
-            win.resize(centerMenuWrapper);
+    //    doc.on('click', function () {
+    //        title.fadeOut(2000);
+    //        doc.off('click');
+    //        win.off('resize');
 
-            menuWrapper.fadeIn(6500);
-            small.fadeOut(1000);
-        });
-    }, 9500)
+    //        centerMenuWrapper();
+    //        win.resize(centerMenuWrapper);
+
+    //        menuWrapper.fadeIn(6500);
+    //        small.fadeOut(1000);
+    //    });
+    //}, 9500)
 
     function centerMenuWrapper() {
         var winWidth = win.width();
         var winHeight = win.height();
         menuWrapper.height(winHeight);
         menuWrapper.width(winWidth);
+        //adjustLeftMarginOfAuthorsWrapper();
     }
 })()
+
+//function adjustLeftMarginOfAuthorsWrapper() {
+//    var authorsWrapper = $('#authors-wrapper');
+//    var awParent = authorsWrapper.parent();
+//    var marginLeft = (awParent.width() - authorsWrapper.width()) / 2;
+
+//    if (marginLeft > 0) {
+//        authorsWrapper.css('margin-left', marginLeft * 0.89);
+//    }
+//}
